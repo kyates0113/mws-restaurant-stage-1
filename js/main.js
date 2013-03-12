38dca7319b3a02147198141804a47f414c78ee09
Kristine Yates <kristine.e.yates@gmail.com>
2013-03-12 18:51:51 -0500

    Added maps API

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1Ijoia3lhdGVzMDExMyIsImEiOiJjamw0Y3Flc2swajU3M3dxOHh2cTAzdjQ0In0.c3bhKF1uW_MzcJHw9Ed4Xw',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
